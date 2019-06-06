5899c37c4ad6c6913936f71ad17740a7febc25ee
zhangrongxin <zhangrongxin@ibeesaas.com>
2019-06-06 11:00:10 +0800

    can run

--- src/main/java/com/ibeesaas/eureka_test/EurekaTestApplication.java
+++ src/main/java/com/ibeesaas/eureka_test/EurekaTestApplication.java
@@ -2,7 +2,9 @@ package com.ibeesaas.eureka_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaTestApplication {
 
